ac0233dd97693bc7003e59aa27b5d9b2acb760af
PatrickMogul <patrick@easynode.pro>
2022-12-16 21:53:01 -0600

    linted

--- api/handler.js
+++ api/handler.js
@@ -1,12 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(
-  request: NextApiRequest,
-  response: NextApiResponse,
-) {
+export default function handler(request, response) {
   response.status(200).json({
     body: request.body,
     query: request.query,
-    cookies: request.cookies,
-  });
+    cookies: request.cookies
+  })
 }
